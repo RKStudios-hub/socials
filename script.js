fd27b7fcf30625ba873aa000c1e1c8e8bfd5fd05
🌈RK_Studios🚀 <hrupeshkumarh@gmail.com>
2024-06-19 12:11:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ document.addEventListener('mousemove', function(e) {
         videoContainer.style.transform = `translate(-50%, -50%) translate(${x}px, ${y}px)`;
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('backgroundMusic');
+    var playPromise = audio.play();
+
+    if (playPromise !== undefined) {
+        playPromise.catch(function(error) {
+            console.log('Autoplay was prevented. Trying to play again after user interaction.');
+            // Wait for user interaction
+            document.body.addEventListener('click', function() {
+                audio.play();
+            }, { once: true });
+        });
+    }
+});
